aa760f8d0ee75d6bc689996d0e6845253b572e04
Homary <935124541@qq.com>
2018-05-09 23:52:32 +0800

    11.0

--- src/main/resources/static/js/url.js
+++ src/main/resources/static/js/url.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-const URL = 'http://82189726.ngrok.io';
-=======
-//const URL = 'http://15f8bdf6.ngrok.io';
-const URL = 'http://127.0.0.1;8080';
->>>>>>> refs/remotes/origin/master
+
+const URL = 'http://b18e5605.ngrok.io';
+
